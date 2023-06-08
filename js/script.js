f1ca7297eabb96a0e0921eb9698b4857edf42923
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-06-08 08:39:03 +0200

    esercizio base

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const { createApp } = Vue; 
+
+createApp({
+    
+    data() {
+        return {
+            hello: '<h1>dormire è importante</h1>',
+           
+        }
+    }
+}).mount('#app');
